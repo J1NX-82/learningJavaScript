3de3dbab7e7a0fb4100008ff85ec3dcec89e3bf8
J1NX-82 <rahulgoala082.gmail.com>
2025-02-22 22:24:38 +0530

    jS

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,11 +1,12 @@
-const name = "hitesh"
+const name = "RAHUL"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
+// this way of writing is called --string interpolation--
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('rahul-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -22,7 +23,7 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   lugia    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
@@ -30,6 +31,6 @@ console.log(newStringOne.trim());
 console.log(gameName.split('-'));
 
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://rahul.com/rahul%20choudhary"
 console.log(url.replace('%20', ''))
 console.log(url.includes('sundar'))
